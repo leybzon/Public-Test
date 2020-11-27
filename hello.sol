2bb5a8115fa0bc6dfd85d8da5fb9ca756aa553d6
Gene Leybzon <leybzon@gmail.com>
2020-11-26 17:59:30 -0800

    Update hello.sol

--- hello.sol
+++ hello.sol
@@ -1,6 +1,4 @@
-// Specifies the version of Solidity, using semantic versioning.
-// Learn more: https://solidity.readthedocs.io/en/v0.5.10/layout-of-source-files.html#pragma
-pragma solidity ^0.5.10;
+pragma solidity >=0.4.22 <0.8.0;
 
 // Defines a contract named `HelloWorld`.
 // A contract is a collection of functions and data (its state).
